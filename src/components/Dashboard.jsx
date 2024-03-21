702fd2671512dc1830993eeb9e78c16d878273de
jose m ruiz <josemruiz23@gmail.com>
2024-03-21 09:45:56 -0300

    Estilo terminado, creando dashboard navbar y sidebar

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className='flex p-4 bg-slate-400 w-screen justify-center '>
+            <div>
+                <button class="bg-cyan-800 hover:bg-slate-950 text-white font-bold py-3 px-6 rounded-full shadow-lg hover:text-white shadow-white transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
+                    Archivo 
+                </button>
+            </div>
+            
+                
+
+        </div>
+    )
+}
+
+export default Dashboard
